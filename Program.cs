d8cf955e6bdc61efc1ee98bd3fa5fe4378b4d20e
RustyAlgorithm <46189013+RustyAlgorithm@users.noreply.github.com>
2025-01-31 13:57:08 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("++ Welcome to the Astra Militarum Regiment Registration System ++");
+Console.WriteLine("Please enter the name of the regiment you wish to register:");
+string regimentName = Console.ReadLine();
+if (regimentName == null || regimentName.Length == 0)
+{
+    Console.WriteLine("Invalid regiment name. Exiting.");
+    return;
+}
+Console.WriteLine("Please select a Homeworld for the regiment:");
+
+string homeworld = Console.ReadLine();
+if (homeworld == null || homeworld.Length == 0)
+{
+    Console.WriteLine("Invalid homeworld. Exiting.");
+    return;
+}
